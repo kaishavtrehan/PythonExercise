c65de6a6d4ea12742462923bd7ea97e5355f19bb
Kaishav Trehan <kaishavtrehan88@gmail.com>
2017-05-23 14:39:39 -0400

    First Commit

---
+++ Fibo-Faster.py
@@ -0,0 +1,6 @@
+def fibonacci(n, computed = {0: 0, 1: 1}):
+    if n not in computed:
+        computed[n] = fibonacci(n-1, computed) + fibonacci(n-2, computed)
+    return computed[n]
+n = int(input())
+print(fibonacci(n))
